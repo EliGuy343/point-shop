f4e08d81426d4c26b7f80661c3a3b711e822f258
Eli Guy <eliguy343@gmail.com>
2022-07-07 14:35:07 +0300

    Created: routes and DB models

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const userRoute = require('./routes/user.js');
 require('dotenv').config();
 
 PORT = process.env.PORT || 8000
@@ -9,6 +10,8 @@ mongoose
   .then(() =>console.log('Sever Successfully connected to database'))
   .catch(err=>console.log(err)); 
 
+app.use(express.json());
+app.use('/api/users', userRoute);
 app.listen(PORT, () => {
   console.log(`backend is running on server ${PORT}`);
 });
